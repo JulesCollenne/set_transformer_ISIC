31fbe69b31206d89a1b5c91a203b8406706aaa3d
JulesCollenne <jules.collenne@univ-amu.fr>
2023-10-27 16:17:11 +0200

    added dataloader per patient

--- data_isic.py
+++ data_isic.py
@@ -35,60 +35,151 @@ def standardize(x):
     return (z - mean) / std
 
 
-class ModelFetcher(object):
-    def __init__(self, train_name, val_name, batch_size, down_sample=10, do_standardize=True, do_augmentation=False):
-
-        self.train_name = train_name
-        self.val_name = val_name
+class DataLoaderISIC:
+    def __init__(self, df_name, gt_name, batch_size, input_dim=20):
         self.batch_size = batch_size
-        self.down_sample = down_sample
+        self.input_dim = input_dim
 
-        self._train_data = pd.read_csv(train_name).filter(like="feature", axis=1)
-        self._train_label = pd.read_csv(train_name)["target"]
-        self._test_data = pd.read_csv(val_name).filter(like="feature", axis=1)
-        self._test_label = pd.read_csv(val_name)["target"]
+        self.gt = pd.read_csv(gt_name)
+        self.train_features = pd.read_csv(df_name)
 
-        self.num_classes = np.max(self._train_label) + 1
+        matching_patient_ids = self.gt[self.gt["image_name"].isin(self.train_features["image_name"])]["patient_id"]
+        self.patients = matching_patient_ids.unique()
+
+    def train_data(self):
+        start = 0
+        end = self.batch_size
+        while end < len(self.patients):
+            current_patients = self.patients[start:end]
+            current_images = [self.gt[self.gt["patient_id"] == pid]["image_name"].values for pid in current_patients]
+            current_features = []
+            current_labels = []
+            for pnum, images in enumerate(current_images):
+                current_patient_features = []
+                current_patient_labels = []
+                for i in range(self.input_dim):
+                    if i < len(images):  # If a patient has more images than needed
+                        current_patient_features.append(
+                            self.train_features[self.train_features["image_name"] == images[i]].filter(like="feature").values[0])
+                        current_patient_labels.append(self.gt[self.gt["image_name"] == images[i]]["target"].values[0])
+                    else:
+                        current_patient_features.append(np.zeros(100))
+                        current_patient_labels.append(0)  # TODO not sre whether I should do something else
+                current_features.append(current_patient_features)
+                current_labels.append(current_patient_labels)
+            current_features = np.asarray(current_features)
+            current_labels = np.asarray(current_labels)
+            yield current_features, current_labels
+            end += self.batch_size
+            start += self.batch_size
+
+
+class ModelFetcherISIC(object):
+    def __init__(self, train_name, val_name, gt_name, batch_size, do_standardize=True, do_augmentation=False):
+        self.train_name = train_name
+        self.batch_size = batch_size
+
+        # Read image names and patient IDs from the train CSV
+        train_data = pd.read_csv(train_name)
+        self._train_data = train_data.filter(like="feature", axis=1).values
+        self._train_data = train_data["target"].values
+        self._train_image_names = train_data["image_name"].values
+
+        # Read the ground truth CSV to map image names to patient IDs
+        gt_data = pd.read_csv(gt_name)
+        self.image_to_patient = dict(zip(gt_data["image_name"], gt_data["patient_id"]))
+
+        # Group image names by patient ID
+        self.patient_images = {}
+        for image_name, patient_id in self.image_to_patient.items():
+            if patient_id not in self.patient_images:
+                self.patient_images[patient_id] = []
+            self.patient_images[patient_id].append(image_name)
+
+        self.num_classes = np.max(train_data["target"]) + 1
 
         self.num_train_batches = len(self._train_data) // self.batch_size
-        self.num_test_batches = len(self._test_data) // self.batch_size
 
         self.prep1 = standardize if do_standardize else lambda x: x
         self.prep2 = (lambda x: augment(self.prep1(x))) if do_augmentation else self.prep1
 
-        assert len(self._train_data) > self.batch_size, \
-            'Batch size larger than number of training examples'
+        self.current_patient = 0
 
-        # select the subset of points to use throughout beforehand
-        self.perm = np.random.permutation(self._train_data.shape[1])[::self.down_sample]
+        assert len(self._train_data) > self.batch_size, 'Batch size larger than the number of training examples'
 
     def train_data(self):
         rng_state = np.random.get_state()
         np.random.shuffle(self._train_data)
         np.random.set_state(rng_state)
-        np.random.shuffle(self._train_label)
+        self.current_patient = 0  # Initialize the current patient
         return self.next_train_batch()
 
     def next_train_batch(self):
         start = 0
         end = self.batch_size
         N = len(self._train_data)
-        perm = self.perm
-        batch_card = len(perm) * np.ones(self.batch_size, dtype=np.int32)
         while end < N:
-            yield self.prep2(self._train_data[start:end, perm]), batch_card, self._train_label[start:end]
-            start = end
-            end += self.batch_size
+            # Get the image names for the current patient
+            patient_id = list(self.patient_images.keys())[self.current_patient]
+            image_names = self.patient_images[patient_id]
 
-    def test_data(self):
-        return self.next_test_batch()
+            # Get the data and labels for the current batch
+            batch_data = [self.prep2(self._train_data[start:end]) for start, end in
+                          zip(range(start, end), range(start + self.batch_size, end + self.batch_size))]
+            batch_labels = self._train_label[start:end]
 
-    def next_test_batch(self):
-        start = 0
-        end = self.batch_size
-        N = len(self._test_data)
-        batch_card = (self._train_data.shape[1] // self.down_sample) * np.ones(self.batch_size, dtype=np.int32)
-        while end < N:
-            yield self.prep1(self._test_data[start:end, 1::self.down_sample]), batch_card, self._test_label[start:end]
-            start = end
-            end += self.batch_size
+            yield batch_data, batch_labels
+
+            # Move to the next patient
+            self.current_patient = (self.current_patient + 1) % len(self.patient_images)
+
+# class ModelFetcherISIC(object):
+#     def __init__(self, train_name, val_name, batch_size, do_standardize=True, do_augmentation=False):
+#
+#         self.train_name = train_name
+#         self.val_name = val_name
+#         self.batch_size = batch_size
+#
+#         self._train_data = np.asarray(pd.read_csv(train_name).filter(like="feature", axis=1).values)
+#         self._train_label = pd.read_csv(train_name)["target"]
+#         self._test_data = np.asarray(pd.read_csv(val_name).filter(like="feature", axis=1).values)
+#         self._test_label = pd.read_csv(val_name)["target"]
+#
+#         self.num_classes = np.max(self._train_label) + 1
+#
+#         self.num_train_batches = len(self._train_data) // self.batch_size
+#         self.num_test_batches = len(self._test_data) // self.batch_size
+#
+#         self.prep1 = standardize if do_standardize else lambda x: x
+#         self.prep2 = (lambda x: augment(self.prep1(x))) if do_augmentation else self.prep1
+#
+#         assert len(self._train_data) > self.batch_size, \
+#             'Batch size larger than number of training examples'
+#
+#     def train_data(self):
+#         rng_state = np.random.get_state()
+#         np.random.shuffle(self._train_data)
+#         np.random.set_state(rng_state)
+#         np.random.shuffle(self._train_label)
+#         return self.next_train_batch()
+#
+#     def next_train_batch(self):
+#         start = 0
+#         end = self.batch_size
+#         N = len(self._train_data)
+#         while end < N:
+#             yield self.prep2(self._train_data[start:end]), self._train_label[start:end]
+#             start = end
+#             end += self.batch_size
+#
+#     def test_data(self):
+#         return self.next_test_batch()
+#
+#     def next_test_batch(self):
+#         start = 0
+#         end = self.batch_size
+#         N = len(self._test_data)
+#         while end < N:
+#             yield self.prep1(self._test_data[start:end]), self._test_label[start:end]
+#             start = end
+#             end += self.batch_size
--- main_pointcloud.py
+++ main_pointcloud.py
@@ -6,39 +6,41 @@ import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 
+from data_isic import ModelFetcherISIC, DataLoaderISIC
 from data_modelnet40 import ModelFetcher
+from models import SetTransformer
 from modules import ISAB, PMA, SAB
 
 
-class SetTransformer(nn.Module):
-    def __init__(
-        self,
-        dim_input=100,
-        num_outputs=1,
-        dim_output=2,
-        num_inds=32,
-        dim_hidden=128,
-        num_heads=4,
-        ln=False,
-    ):
-        super(SetTransformer, self).__init__()
-        self.enc = nn.Sequential(
-            ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
-            ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
-        )
-        self.dec = nn.Sequential(
-            nn.Dropout(),
-            PMA(dim_hidden, num_heads, num_outputs, ln=ln),
-            nn.Dropout(),
-            nn.Linear(dim_hidden, dim_output),
-        )
-
-    def forward(self, X):
-        return self.dec(self.enc(X)).squeeze()
+# class SetTransformer(nn.Module):
+#     def __init__(
+#         self,
+#         dim_input=100,
+#         num_outputs=1,
+#         dim_output=2,
+#         num_inds=32,
+#         dim_hidden=128,
+#         num_heads=4,
+#         ln=False,
+#     ):
+#         super(SetTransformer, self).__init__()
+#         self.enc = nn.Sequential(
+#             ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
+#             ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+#         )
+#         self.dec = nn.Sequential(
+#             nn.Dropout(),
+#             PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+#             nn.Dropout(),
+#             nn.Linear(dim_hidden, dim_output),
+#         )
+#
+#     def forward(self, X):
+#         return self.dec(self.enc(X)).squeeze()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--num_pts", type=int, default=1000)
+# parser.add_argument("--num_pts", type=int, default=1000)
 parser.add_argument("--learning_rate", type=float, default=1e-3)
 parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--dim", type=int, default=256)
@@ -46,20 +48,29 @@ parser.add_argument("--n_heads", type=int, default=4)
 parser.add_argument("--n_anc", type=int, default=16)
 parser.add_argument("--train_epochs", type=int, default=2000)
 args = parser.parse_args()
-args.exp_name = f"N{args.num_pts}_d{args.dim}h{args.n_heads}i{args.n_anc}_lr{args.learning_rate}bs{args.batch_size}"
+args.exp_name = f"Nd{args.dim}h{args.n_heads}i{args.n_anc}_lr{args.learning_rate}bs{args.batch_size}"
 log_dir = "result/" + args.exp_name
 model_path = log_dir + "/model"
 writer = SummaryWriter(log_dir)
 
-generator = ModelFetcher(
-    "../dataset/ModelNet40_cloud.h5",
-    args.batch_size,
-    down_sample=int(10000 / args.num_pts),
-    do_standardize=True,
-    do_augmentation=(args.num_pts == 5000),
+# generator = ModelFetcherISIC(
+#     "features/simsiam_train.csv",
+#     "features/simsiam_val.csv",
+#     "GroundTruth.csv",
+#     args.batch_size,
+#     do_standardize=False,
+#     do_augmentation=False,
+# )
+
+
+generator = DataLoaderISIC(
+    "features/simsiam_train.csv",
+    "GroundTruth.csv",
+    batch_size=args.batch_size,
 )
 
-model = SetTransformer(dim_hidden=args.dim, num_heads=args.n_heads, num_inds=args.n_anc)
+# model = SetTransformer(dim_hidden=args.dim, num_heads=args.n_heads, num_inds=args.n_anc)
+model = SetTransformer(100, 2, 2, num_inds=20)
 optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 criterion = nn.CrossEntropyLoss()
 model = nn.DataParallel(model)
@@ -68,7 +79,9 @@ model = model.cuda()
 for epoch in range(args.train_epochs):
     model.train()
     losses, total, correct = [], 0, 0
-    for imgs, _, lbls in generator.train_data():
+    for imgs, lbls in generator.train_data():
+        print(imgs)
+        print(imgs.shape)
         imgs = torch.Tensor(imgs).cuda()
         lbls = torch.Tensor(lbls).long().cuda()
         preds = model(imgs)
@@ -90,7 +103,7 @@ for epoch in range(args.train_epochs):
     if epoch % 10 == 0:
         model.eval()
         losses, total, correct = [], 0, 0
-        for imgs, _, lbls in generator.test_data():
+        for imgs, lbls in generator.test_data():
             imgs = torch.Tensor(imgs).cuda()
             lbls = torch.Tensor(lbls).long().cuda()
             preds = model(imgs)
